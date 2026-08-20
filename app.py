60a5eaa8c036ff0f2cef938913f45145cd341b36
saimonmesadiaz-dotcom <saimonmesadiaz@gmail.com>
2026-08-20 14:25:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,6 @@ st.write('Un amor que se ganó con valentía. En lo alto de los árboles, una ba
          'a otro macho por el amor de ella, y aunque no fue el más fuerte, metafóricamente fue quien mostró'
          'su mejor versión. Hoy comparten una rama, un refugio y la tranquilidad de estar juntos. La imagen'
          'representa el amor, el valor y la compañía, recordándonos que, a veces, el amor no se encuentra… se conquista.'
-         '
-         
          'Esta fotografía fue tomada el 19 de agosto de 2026, al salir de la universidad mientras regresaba a casa, en'
          'un encuentro inesperado con una pequeña historia de amor en medio del camino. ' 
          '  '
