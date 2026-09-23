dd2d51145c931245cf4407df21feac365ffc19f2
saimonmesadiaz-dotcom <saimonmesadiaz@gmail.com>
2026-09-23 10:47:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import base64
 st.title("Conversión de Texto a Audio")
 image = Image.open('LiebreTortuga.jpg')
 st.image(image, width=350)
-with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
-
 
 try:
     os.mkdir("temp")
